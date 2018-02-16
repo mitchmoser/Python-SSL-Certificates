32429ec7788ea8861d80f92bf0b31f9ce1881b4c
Mitchell Moser <moser.mitch@yahoo.com>
2018-02-16 16:35:23 +0000

    Update newCA.py

--- newCA.py
+++ newCA.py
@@ -1,6 +1,7 @@
-#!/usr/bin/env python3import ssl
-context = ssl.create_default_context()
+#!/usr/bin/env python3
+import ssl
 
+context = ssl.create_default_context()
 der_certs = context.get_ca_certs(binary_form=True)
 pem_certs = [ssl.DER_cert_to_PEM_cert(der) for der in der_certs]
 
