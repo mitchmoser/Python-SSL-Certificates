6e9d0a2b24fdd3584a0b8ba89484e69cf6be7ca6
Mitchell Moser <moser.mitch@yahoo.com>
2018-02-16 16:34:31 +0000

    Create newCA.py

---
+++ newCA.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3import ssl
+context = ssl.create_default_context()
+der_certs = context.get_ca_certs(binary_form=True)
+pem_certs = [ssl.DER_cert_to_PEM_cert(der) for der in der_certs]
+with open('wincacerts.pem', 'w') as outfile:
+    for pem in pem_certs:
+        outfile.write(pem + '\n')
